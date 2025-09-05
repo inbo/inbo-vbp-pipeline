a1554da11dccb0e1d25cc7c37c7d3d5e53c7d139
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-09-05 18:31:34 +0200

    inbo/vlaams-biodiversiteitsportaal#42: Revert accidental addition of superfluous aws provider

--- terraform/region/lambda_solr_management.tf
+++ terraform/region/lambda_solr_management.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.81.0"
-    }
-  }
-}
 resource "aws_lb_target_group" "solr_management_lambda" {
   name        = "inbo-${var.application}-solr-management"
   target_type = "lambda"
