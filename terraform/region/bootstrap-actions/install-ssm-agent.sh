cefb8112de4d66e0f2939f923e82b39b34d436b9
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-01 15:41:20 +0200

    inbo-vlaamsbiodiversiteitsportaal#42: Try fixing emr bootstrap steps

--- terraform/region/bootstrap-actions/install-ssm-agent.sh
+++ terraform/region/bootstrap-actions/install-ssm-agent.sh
@@ -7,3 +7,5 @@ set -e -o pipefail
 sudo yum install -y https://s3.eu-west-1.amazonaws.com/amazon-ssm-eu-west-1/latest/linux_arm64/amazon-ssm-agent.rpm
 sudo systemctl start amazon-ssm-agent
 sudo systemctl status amazon-ssm-agent
+
+sudo ssm-cli get-diagnostics --output table
