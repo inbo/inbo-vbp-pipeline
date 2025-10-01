7a95c4d092801dfac0c1e434a519df7a198a9136
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-01 16:43:21 +0200

    inbo-vlaamsbiodiversiteitsportaal#42: Fix EMR cluster EFS mount

--- terraform/region/bootstrap-actions/install-ssm-agent.sh
+++ terraform/region/bootstrap-actions/install-ssm-agent.sh
@@ -6,6 +6,3 @@ set -e -o pipefail
 
 sudo yum install -y https://s3.eu-west-1.amazonaws.com/amazon-ssm-eu-west-1/latest/linux_arm64/amazon-ssm-agent.rpm
 sudo systemctl start amazon-ssm-agent
-sudo systemctl status amazon-ssm-agent
-
-sudo ssm-cli get-diagnostics --output table
--- terraform/region/bootstrap-actions/mount-efs.sh
+++ terraform/region/bootstrap-actions/mount-efs.sh
@@ -3,10 +3,9 @@ set -e -o pipefail
 
 EFS_VOLUME_ID=${1:?EFS_VOLUME_ID is required as first argument}
 EFS_ACCESS_POINT_ID=${2:?EFS_ACCESS_POINT_ID is required as second argument}
-MOUNT_PORT=${3:?MOUNT_PORT is required as third argument}
-MOUNT_POINT=${4:?MOUNT_POINT is required as fourth argument}
+MOUNT_POINT=${3:?MOUNT_POINT is required as third argument}
 
 sudo yum install -y amazon-efs-utils
 
 sudo mkdir -p "${MOUNT_POINT}"
-sudo mount -t efs -o tls,accesspoint="${EFS_ACCESS_POINT_ID}",mountport=${MOUNT_PORT} "${EFS_VOLUME_ID}" "${MOUNT_POINT}"
+sudo mount -t efs -o tls,accesspoint="${EFS_ACCESS_POINT_ID}" "${EFS_VOLUME_ID}" "${MOUNT_POINT}"
