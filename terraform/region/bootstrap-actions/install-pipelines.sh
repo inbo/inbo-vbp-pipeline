d08ef0212fa47788348b34595da99e75ef090992
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-01 18:22:38 +0200

    inbo-vlaamsbiodiversiteitsportaal#42:Only get specific version of pipeline jar

--- terraform/region/bootstrap-actions/install-pipelines.sh
+++ terraform/region/bootstrap-actions/install-pipelines.sh
@@ -5,12 +5,16 @@ APIKEY_SECRET_ARN=${1:?No APIKEY_SECRET_ARN provided}
 PIPELINE_BUCKET_NAME=${2:?No PIPELINE_BUCKET_NAME provided}
 PIPELINE_VERSION=${3:?No PIPELINE_VERSION provided}
 
-sudo mkdir -p /opt/inbo/pipelines/config
-sudo mkdir -p /opt/inbo/pipelines/shp-layers
-
 sudo aws s3 cp "s3://${PIPELINE_BUCKET_NAME}/pipelines-${PIPELINE_VERSION}.jar" "/opt/inbo/pipelines/pipelines-${PIPELINE_VERSION}.jar"
+
+sudo mkdir -p /opt/inbo/pipelines/config
 sudo aws s3 sync "s3://${PIPELINE_BUCKET_NAME}/config" /opt/inbo/pipelines/config
+
+sudo mkdir -p /opt/inbo/pipelines/shp-layers
 sudo aws s3 sync "s3://${PIPELINE_BUCKET_NAME}/shp-layers" /opt/inbo/pipelines/shp-layers
+
+sudo mkdir -p /opt/inbo/pipelines/pipelines-vocabularies
+sudo aws s3 sync "s3://${PIPELINE_BUCKET_NAME}/pipelines-vocabularies" /opt/inbo/pipelines/pipelines-vocabularies
 
 sudo chown -R hadoop:yarn /opt/inbo/pipelines
 
