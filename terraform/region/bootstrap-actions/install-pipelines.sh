bef0135a0a167b85a3531314d6e6b628e451663b
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-02 13:18:23 +0200

    inbo-vlaamsbiodiversiteitsportaal#42: pipeline install

--- terraform/region/bootstrap-actions/install-pipelines.sh
+++ terraform/region/bootstrap-actions/install-pipelines.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 set -e -x -o pipefail
 
-APIKEY_SECRET_ARN=${1:?No APIKEY_SECRET_ARN provided}
-PIPELINE_BUCKET_NAME=${2:?No PIPELINE_BUCKET_NAME provided}
-PIPELINE_VERSION=${3:?No PIPELINE_VERSION provided}
+#APIKEY_SECRET_ARN=${1:?No APIKEY_SECRET_ARN provided}
+#PIPELINE_BUCKET_NAME=${2:?No PIPELINE_BUCKET_NAME provided}
+#PIPELINE_VERSION=${3:?No PIPELINE_VERSION provided}
 
-sudo aws s3 cp "s3://${PIPELINE_BUCKET_NAME}/pipelines-${PIPELINE_VERSION}.jar" "/opt/inbo/pipelines/pipelines-${PIPELINE_VERSION}.jar"
-
-sudo mkdir -p /opt/inbo/pipelines/config
-sudo aws s3 sync "s3://${PIPELINE_BUCKET_NAME}/config" /opt/inbo/pipelines/config
-
-sudo mkdir -p /opt/inbo/pipelines/shp-layers
-sudo aws s3 sync "s3://${PIPELINE_BUCKET_NAME}/shp-layers" /opt/inbo/pipelines/shp-layers
-
-sudo mkdir -p /opt/inbo/pipelines/pipelines-vocabularies
-sudo aws s3 sync "s3://${PIPELINE_BUCKET_NAME}/pipelines-vocabularies" /opt/inbo/pipelines/pipelines-vocabularies
-
-sudo mkdir -p /opt/inbo/pipelines/bootstrap-actions
-sudo aws s3 sync "s3://${PIPELINE_BUCKET_NAME}/bootstrap-actions" /opt/inbo/pipelines/bootstrap-actions
-
+sudo aws s3 sync --delete \
+  --exclude "*" \
+  --include "pipelines-${PIPELINE_VERSION}.jar" \
+  --include "config/*" \
+  --include "shp-layers/*" \
+  --include "pipelines-vocabularies/*" \
+  --include "bootstrap-actions/*" \
+  "s3://${PIPELINE_BUCKET_NAME}" /opt/inbo/pipelines
 
 sudo chown -R hadoop:yarn /opt/inbo/pipelines
-sudo chmod +x -R /opt/inbo/pipelines/boostrap-actions
+sudo chmod +x -R /opt/inbo/pipelines/bootstrap-actions
 sudo chmod +x "/opt/inbo/pipelines/pipelines-${PIPELINE_VERSION}.jar"
 
 mkdir -p /data/dwca-exports
