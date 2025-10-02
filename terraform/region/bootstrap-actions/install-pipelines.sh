c927ac9bf899d432316b264148b639b58e36dbfd
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-02 13:18:34 +0200

    inbo-vlaamsbiodiversiteitsportaal#42: pipeline install

--- terraform/region/bootstrap-actions/install-pipelines.sh
+++ terraform/region/bootstrap-actions/install-pipelines.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e -x -o pipefail
 
-#APIKEY_SECRET_ARN=${1:?No APIKEY_SECRET_ARN provided}
-#PIPELINE_BUCKET_NAME=${2:?No PIPELINE_BUCKET_NAME provided}
-#PIPELINE_VERSION=${3:?No PIPELINE_VERSION provided}
+APIKEY_SECRET_ARN=${1:?No APIKEY_SECRET_ARN provided}
+PIPELINE_BUCKET_NAME=${2:?No PIPELINE_BUCKET_NAME provided}
+PIPELINE_VERSION=${3:?No PIPELINE_VERSION provided}
 
 sudo aws s3 sync --delete \
   --exclude "*" \
