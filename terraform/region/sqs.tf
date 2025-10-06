3bc61c51c5042de02f42423d25612cd190688173
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-06 13:09:22 +0200

    inbo-vlaamsbiodiversiteitsportaal#42: fix tf syntax

--- terraform/region/sqs.tf
+++ terraform/region/sqs.tf
@@ -1,8 +1,8 @@
 #tfsec:ignore:aws-sqs-enable-queue-encryption
 resource "aws_sqs_queue" "lock-queues" {
-  for_each = toset(["sample, solr"])
+  for_each = toset(["sample", "solr"])
 
   name = "${var.resource_prefix}pipeline-lock-${each.value}"
 
-  message_retention_seconds   = 60
+  message_retention_seconds = 60
 }
