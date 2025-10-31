b9f7fa9b4eef7671903558222008a5254fcd5cc6
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-10-31 09:24:54 +0100

    disable schedule for now

--- terraform/region/schedule.tf
+++ terraform/region/schedule.tf
@@ -1,15 +1,15 @@
-resource "aws_scheduler_schedule" "weekly_indexing" {
-  name       = "Schedule-indexing"
-  group_name = var.resource_prefix
-
-  flexible_time_window {
-    mode = "OFF"
-  }
-
-  schedule_expression = "cron(0 0 * * SAT)"
-
-  target {
-    arn      = aws_sfn_state_machine.pipeline.arn
-    role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.resource_prefix}pipeline-step-function"
-  }
-}
+# resource "aws_scheduler_schedule" "weekly_indexing" {
+#   name       = "Schedule-indexing"
+#   group_name = var.resource_prefix
+#
+#   flexible_time_window {
+#     mode = "OFF"
+#   }
+#
+#   schedule_expression = "cron(0 0 * * SAT)"
+#
+#   target {
+#     arn      = aws_sfn_state_machine.pipeline.arn
+#     role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.resource_prefix}pipeline-step-function"
+#   }
+# }
