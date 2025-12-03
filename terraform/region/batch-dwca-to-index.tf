d586ce588c538ea703a6e5b656d14d4c787ddb8e
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-12-03 17:06:59 +0100

    make s3 bucket configurable

--- terraform/region/batch-dwca-to-index.tf
+++ terraform/region/batch-dwca-to-index.tf
@@ -29,32 +29,10 @@ resource "aws_batch_job_definition" "la_pipelines" {
       }
     ],
     command = [
-      <<-EOT
-        #!/usr/bin/env bash
-        set -e -x -o pipefail
-
-        mkdir -p /data/pipelines-shp
-        mkdir -p /tmp/pipelines && chmod 777 /tmp/pipelines
-
-        aws s3 sync s3://${aws_s3_bucket.pipelines.bucket}/shp-layers/ /data/pipelines-shp
-        aws s3 sync s3://${aws_s3_bucket.pipelines.bucket}/pipelines-vocabularies/ /data/pipelines-vocabularies
-
-        aws s3 cp s3://${aws_s3_bucket.pipelines.bucket}/${aws_s3_object.batch_pipelines_config.id} ../configs/la-pipelines.yaml
-        aws s3 cp s3://${aws_s3_bucket.pipelines.bucket}/${aws_s3_object.batch_pipelines_log_config.id} ../configs/log4j.properties
-        sed -i "s\\\$${APIKEY}\\$${APIKEY}\\g" ../configs/la-pipelines.yaml
-
-        export LOG_CONFIG="/app/livingatlas/pipelines/src/main/resources/log4j.properties"
-
-        $(aws configure --debug export-credentials | yq -r '"export AWS_ACCESS_KEY_ID=" + .AccessKeyId + "\nexport AWS_SECRET_ACCESS_KEY="  + .SecretAccessKey + "\nexport AWS_SESSION_TOKEN=" + .SessionToken')
-
-        ./la-pipelines dwca-avro                            $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1,fsPath=s3://${aws_s3_bucket.pipelines.bucket}/data
-        ./la-pipelines interpret  --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1,fsPath=s3://${aws_s3_bucket.pipelines.bucket}/data
-        ./la-pipelines validate   --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1,fsPath=s3://${aws_s3_bucket.pipelines.bucket}/data
-        ./la-pipelines uuid       --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1,fsPath=s3://${aws_s3_bucket.pipelines.bucket}/data
-        ./la-pipelines image-sync --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1,fsPath=s3://${aws_s3_bucket.pipelines.bucket}/data
-        ./la-pipelines image-load --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1,fsPath=s3://${aws_s3_bucket.pipelines.bucket}/data
-        ./la-pipelines index      --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1,fsPath=s3://${aws_s3_bucket.pipelines.bucket}/data
- EOT
+      <<EOF
+aws s3 cp s3://inbo-vbp-dev-pipelines/bootstrap-actions/index-data-resource.sh index-data-resource.sh
+bash index-data-resource.sh
+EOF
     ]
     image      = "${var.ecr_repo}/${var.resource_prefix}pipelines:${var.docker_version}"
     jobRoleArn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.resource_prefix}pipelines-batch-role"
--- terraform/region/bootstrap-actions/index-data-resource.sh
+++ terraform/region/bootstrap-actions/index-data-resource.sh
@@ -1,11 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e -x -o pipefail
 
-DATA_RESOURCE_ID=${1:?DATA_RESOURCE_ID is required as first argument}
-COMPUTE_ENVIRONMENT=${2:?cluster}
+mkdir -p /data/pipelines-shp
+mkdir -p /tmp/pipelines && chmod 777 /tmp/pipelines
 
-./la-pipelines dwca-avro  ${DATA_RESOURCE_ID}
-./la-pipelines interpret  --${COMPUTE_ENVIRONMENT} ${DATA_RESOURCE_ID}
-./la-pipelines validate   --${COMPUTE_ENVIRONMENT} ${DATA_RESOURCE_ID}
-./la-pipelines image-sync --${COMPUTE_ENVIRONMENT} ${DATA_RESOURCE_ID}
-./la-pipelines index      --${COMPUTE_ENVIRONMENT} ${DATA_RESOURCE_ID}
+aws s3 sync s3://${aws_s3_bucket.pipelines.bucket}/shp-layers/ /data/pipelines-shp
+aws s3 sync s3://${aws_s3_bucket.pipelines.bucket}/pipelines-vocabularies/ /data/pipelines-vocabularies
+
+aws s3 cp s3://${aws_s3_bucket.pipelines.bucket}/${aws_s3_object.batch_pipelines_config.id} ../configs/la-pipelines.yaml
+aws s3 cp s3://${aws_s3_bucket.pipelines.bucket}/${aws_s3_object.batch_pipelines_log_config.id} ../configs/log4j.properties
+sed -i "s\\\$${APIKEY}\\$${APIKEY}\\g" ../configs/la-pipelines.yaml
+sed -i "s\\\inbo-vbp-dev-pipelines\\${aws_s3_bucket.pipelines.bucket}\\g" ../configs/la-pipelines.yaml
+
+export LOG_CONFIG="/app/livingatlas/pipelines/src/main/resources/log4j.properties"
+
+$(aws configure --debug export-credentials | yq -r '"export AWS_ACCESS_KEY_ID=" + .AccessKeyId + "\nexport AWS_SECRET_ACCESS_KEY="  + .SecretAccessKey + "\nexport AWS_SESSION_TOKEN=" + .SessionToken')
+
+./la-pipelines dwca-avro                            $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1
+./la-pipelines interpret  --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1
+./la-pipelines validate   --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1
+./la-pipelines uuid       --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1
+
+MULTI_MEDIA_RECORDS=$(aws s3 cp s3://inbo-vbp-dev-pipelines/data/pipelines-data/dr45/0/interpretation-metrics.yml - | grep multimediaRecordsCountAttempted | awk '{ print $2}' || echo '0')
+if [ $MULTI_MEDIA_RECORDS -gt 0 ]; then
+  echo "Detected Multimedia Records ${MULTI_MEDIA_RECORDS}"
+  ./la-pipelines image-sync --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1
+  ./la-pipelines image-load --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1
+fi
+
+./la-pipelines index      --$${COMPUTE_ENVIRONMENT} $${DATA_RESOURCE_ID} --config ../configs/la-pipelines.yaml --extra-args=awsRegion=eu-west-1
+
