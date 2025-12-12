04cc430deb5e6b8e7e0b17f1e90187ef62d47691
Stefan Van Dyck <vandyck.stefan@gmail.com>
2025-12-12 17:26:44 +0100

    Add refresh token to pipeline frontend

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -4,11 +4,8 @@ export const oidcConfig = {
     authority: settings.auth.authority,
     client_id: settings.auth.client_id,
     redirect_uri: settings.auth.redirectUrl,
+    scope: "openid email ala/roles offline_access",
     onSigninCallback: (): void => {
-        window.history.replaceState(
-            {},
-            document.title,
-            window.location.pathname,
-        );
+        window.history.replaceState({}, document.title, window.location.pathname);
     },
 };
