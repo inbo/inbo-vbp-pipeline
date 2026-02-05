0154806a8b4ccf1086ffd22ad075af5e10056a41
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-02-05 23:34:22 +0100

    Add dark mode to pipeline management UI

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -2,6 +2,10 @@ import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
     cssVariables: {
+        colorSchemeSelector: "data",
         nativeColor: true,
+    },
+    colorSchemes: {
+        dark: true,
     },
 });
