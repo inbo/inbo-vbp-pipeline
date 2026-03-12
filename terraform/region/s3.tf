0616a419c730d72540b7690a37dd64a8fe1b64e0
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-03-12 18:15:33 +0100

    Trye fixing ZK host config var

--- terraform/region/s3.tf
+++ terraform/region/s3.tf
@@ -16,11 +16,12 @@ resource "aws_s3_bucket_public_access_block" "biodiversiteitsportaal_downloads" 
   restrict_public_buckets = true
 }
 
+
 resource "aws_s3_object" "emr_pipelines_config" {
-  bucket = aws_s3_bucket.pipelines.bucket
-  key    = "config/la-pipelines.yaml"
-  source = "${path.module}/config/la-pipelines.yaml"
-  etag   = filemd5("${path.module}/config/la-pipelines.yaml")
+  bucket  = aws_s3_bucket.pipelines.bucket
+  key     = "config/la-pipelines.yaml"
+  content = replace(file("${path.module}/config/la-pipelines.yaml"), "${SOLR_ZK_HOST}", var.solr.zk_host)
+  etag    = filemd5("${path.module}/config/la-pipelines.yaml")
 }
 
 resource "aws_s3_object" "batch_pipelines_config" {
